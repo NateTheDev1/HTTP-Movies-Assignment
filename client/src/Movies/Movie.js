7d4e2607767895d43b005ef13986a189754fcf8a
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-16 17:15:52 -0400

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
+  const history = useHistory();
   const [movie, setMovie] = useState(null);
   const params = useParams();
 
@@ -22,6 +23,14 @@ function Movie({ addToSavedList }) {
     fetchMovie(params.id);
   }, [params.id]);
 
+  const handleDelete = () => {
+    axios
+      .delete(`http://localhost:5000/api/movies/${params.id}`)
+      .then((res) => {
+        history.push("/");
+      });
+  };
+
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
@@ -33,6 +42,10 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button onClick={() => history.push(`/update-movie/${params.id}`)}>
+        Edit
+      </button>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
